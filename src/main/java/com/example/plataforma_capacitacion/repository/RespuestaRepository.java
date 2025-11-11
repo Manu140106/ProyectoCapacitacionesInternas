e8586d7dfda267fe8a05de2b831c2a49e4023c66
Manu140106 <camacho.manuela.7349@eam.edu.co>
2025-11-10 21:03:52 -0500

    Limpieza completa del repositorio

--- src/main/java/com/example/plataforma_capacitacion/repository/RespuestaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.plataforma_capacitacion.repository;
-
-import com.example.plataforma_capacitacion.model.Respuesta;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface RespuestaRepository extends JpaRepository<Respuesta, Long> {
-    List<Respuesta> findByEvaluacionId(Long evaluacionId);
-}
